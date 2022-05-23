d9d5fac95a0fd5f478c77d6d3f208d34d02a5d9d
Challa-Akshay-Santoshi <102526408+Challa-Akshay-Santoshi@users.noreply.github.com>
2022-05-23 19:00:43 +0530

    Add files via upload

---
+++ Assignment-9/Codes/main92.py
@@ -0,0 +1,23 @@
+# C. Akshay Santoshi
+# CS21BTECH11012
+
+# Cummulative Distribution Function
+
+from scipy.stats import binom
+import numpy as np
+import matplotlib.pyplot as plt
+
+# A die is thrown 6 times and success is 'getting an odd number'
+n = 6
+p = 0.5
+
+# PMF Values
+rv = binom(n,p)
+x = np.arange(0,7,1)
+y = rv.cdf(x)
+
+# Plotting the graph
+plt.plot(x, y, 'ro')
+plt.vlines(x,0,y,colors = 'k', label = 'Probability')
+plt.legend()
+plt.show()
