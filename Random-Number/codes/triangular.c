5476f945ba44ebcad80635391bc91167e164861b
Challa-Akshay-Santoshi <102526408+Challa-Akshay-Santoshi@users.noreply.github.com>
2022-07-01 19:23:41 +0530

    Add files via upload

---
+++ Random-Number/codes/triangular.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "triangular.h"
+
+int main()
+{
+    triangular("triangular.dat", 1000000);
+}
---
+++ Random-Number/codes/triangular.h
@@ -0,0 +1,13 @@
+void triangular(char *str, int len)
+{
+    int i;
+    FILE *fp;
+
+    fp = fopen(str, "w");
+
+    for(i=0; i<len; i++)
+    {
+        fprintf(fp, "%lf\n", ((double)rand()+(double)rand())/RAND_MAX);
+    }
+    fclose(fp);
+}
