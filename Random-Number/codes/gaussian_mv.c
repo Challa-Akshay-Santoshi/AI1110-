e40f366b8a733023a157a84afb4cbaa337db4c34
Challa-Akshay-Santoshi <102526408+Challa-Akshay-Santoshi@users.noreply.github.com>
2022-07-04 22:08:09 +0530

    Delete gaussian_mv.c

--- Random-Number/codes/gaussian_mv.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include "gaussian_mv.h"
-
-int main()
-{
-    printf("Mean = %lf\n",mean("gau.dat"));
-    printf("Variance = %lf\n",variance("gau.dat", mean("gau.dat")));
-}
