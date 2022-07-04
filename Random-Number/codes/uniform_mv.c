a7e07922f8e35a86ebad0886adee05800fe7baeb
Challa-Akshay-Santoshi <102526408+Challa-Akshay-Santoshi@users.noreply.github.com>
2022-07-04 22:09:33 +0530

    Delete uniform_mv.c

--- Random-Number/codes/uniform_mv.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include "uniform_mv.h"
-
-int main()
-{
-    printf("Mean = %lf\n",mean("uni.dat"));
-    printf("Variance = %lf\n",variance("uni.dat", mean("uni.dat")));
-}
